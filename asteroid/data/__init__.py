2071c43f529ee0f8c24471e9b8260b1a60be9c4c
jaechang <jaechang@postech.ac.kr>
2022-02-17 13:51:18 +0900

    Remove unnecessary

--- asteroid/data/__init__.py
+++ asteroid/data/__init__.py
@@ -1,31 +1,7 @@
-from .avspeech_dataset import AVSpeechDataset
-from .wham_dataset import WhamDataset
-from .whamr_dataset import WhamRDataset
-from .dns_dataset import DNSDataset
-from .librimix_dataset import LibriMix
-from .wsj0_mix import Wsj0mixDataset
-from .musdb18_dataset import MUSDB18Dataset
 from .mimii_dataset import MIMIIDataset
 from .mimii_single_dataset import MIMIISingleDataset
-from .sms_wsj_dataset import SmsWsjDataset
-from .kinect_wsj import KinectWsjMixDataset
-from .fuss_dataset import FUSSDataset
-from .dampvsep_dataset import DAMPVSEPSinglesDataset
-from .vad_dataset import LibriVADDataset
 
 __all__ = [
-    "AVSpeechDataset",
-    "WhamDataset",
-    "WhamRDataset",
-    "DNSDataset",
-    "LibriMix",
-    "Wsj0mixDataset",
-    "MUSDB18Dataset",
     "MIMIIDataset",
     "MIMIISingleDataset",
-    "SmsWsjDataset",
-    "KinectWsjMixDataset",
-    "FUSSDataset",
-    "DAMPVSEPSinglesDataset",
-    "LibriVADDataset",
 ]
