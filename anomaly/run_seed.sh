d05933802d344a8cc6935297d85af2c6333a064c
yunjoo <yunjoojenn@gmail.com>
2022-10-13 21:39:25 +0900

    Remove comma in run_seed.sh

--- anomaly/run_seed.sh
+++ anomaly/run_seed.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-SEEDS=(0, 1, 2, 3)
+SEEDS=(42 10 1 100 20 27 50 30 14 5)
 
 for seed in ${SEEDS[@]}; do
    RESULT_NAME="slider_id00_02_original_seed${seed}.yaml"
    sed -i "s/^seed.*/seed: ${seed}/g" baseline.yaml
    sed -i "s/^result_file.*/result_file: ${RESULT_NAME}/g" baseline.yaml
-   python baseline_src_xumx_shared.py
+   python baseline_src_xumx_original.py
 done
