74c9c00db5b6c3b0d9aa2259c99912d4181c5182
jaechang <jaechang@postech.ac.kr>
2022-10-22 00:08:07 +0900

    Update scripts

--- anomaly/run_sep.sh
+++ anomaly/run_sep.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
+set -e
 
-SEEDS=(0)
+SEEDS=(42 10 1 100 20 27 50 30 14 5)
 MACHINES=("valve" "slider")
 GPU=5
 
 export CUDA_VISIBLE_DEVICES=$GPU
 PYTHON="/home/kjc/.conda/envs/asteroid/bin/python"
+PYTHON_FILE="baseline_src_xumx_original.py"
 
 for seed in ${SEEDS[@]}; do
    for MACHINE in ${MACHINES[@]}; do
-      sed -i "s/^seed.*/seed: ${seed}/g" baseline.yaml
+      sed -i "s@^seed.*@seed: ${seed}@g" baseline.yaml
 
-      RESULT_DIR="result_1015_baseline"
+      RESULT_DIR="result_1022_dilate_label"
       mkdir -p ${RESULT_DIR}
-      sed -i "s/^result_directory.*/result_directory: ${RESULT_DIR}/g" baseline.yaml
+      sed -i "s@^result_directory.*@result_directory: ${RESULT_DIR}@g" baseline.yaml
 
-      RESULT_NAME="sep_ad_${MACHINE}02_seed${seed}.yaml"
-      sed -i "s/^result_file.*/result_file: ${RESULT_NAME}/g" baseline.yaml
+      RESULT_NAME="ssad_${MACHINE}_seed${seed}.yaml"
+      sed -i "s@^result_file.*@result_file: ${RESULT_NAME}@g" baseline.yaml
 
-      sed -i "s/^MACHINE =.*/MACHINE = '${MACHINE}'/g" baseline_src_xumx_original.py
-      sed -i "s/^S1 =.*/S1 = 'id_00'/g" baseline_src_xumx_original.py
-      sed -i "s/^S2 =.*/S2 = 'id_02'/g" baseline_src_xumx_original.py
+      sed -i "s@^MACHINE =.*@MACHINE = '${MACHINE}'@g" ${PYTHON_FILE}
+      sed -i "s@^S1 =.*@S1 = 'id_00'@g" ${PYTHON_FILE}
+      sed -i "s@^S2 =.*@S2 = 'id_02'@g" ${PYTHON_FILE}
       
-      $PYTHON baseline_src_xumx_original.py
+      $PYTHON ${PYTHON_FILE}
    done
 done
--- anomaly/run_single.sh
+++ anomaly/run_single.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
+set -e
 
-SEEDS=(0)
+SEEDS=(42 10 1 100 20 27 50 30 14 5)
 MACHINES=("valve" "slider")
 GPU=7
 
 export CUDA_VISIBLE_DEVICES=$GPU
 PYTHON="/home/kjc/.conda/envs/asteroid/bin/python"
+PYTHON_FILE="baseline.py"
 
 for seed in ${SEEDS[@]}; do
    for MACHINE in ${MACHINES[@]}; do
-      sed -i "s/^seed.*/seed: ${seed}/g" baseline.yaml
+      sed -i "s@^seed.*@seed: ${seed}@g" baseline.yaml
 
-      RESULT_DIR="result_1015_baseline"
+      RESULT_DIR="result_1022_dilate_label"
       mkdir -p ${RESULT_DIR}
-      sed -i "s/^result_directory.*/result_directory: ${RESULT_DIR}/g" baseline.yaml
+      sed -i "s@^result_directory.*@result_directory: ${RESULT_DIR}@g" baseline.yaml
 
       RESULT_NAME="oracle_baseline_${MACHINE}_seed${seed}.yaml"
-      sed -i "s/^result_file.*/result_file: ${RESULT_NAME}/g" baseline.yaml
+      sed -i "s@^result_file.*@result_file: ${RESULT_NAME}@g" baseline.yaml
 
-      sed -i "s/^MACHINE =.*/MACHINE = '${MACHINE}'/g" baseline.py
+      sed -i "s@^MACHINE =.*@MACHINE = '${MACHINE}'@g" ${PYTHON_FILE}
 
-      $PYTHON baseline.py
+      ${PYTHON} ${PYTHON_FILE}
    done
 done
